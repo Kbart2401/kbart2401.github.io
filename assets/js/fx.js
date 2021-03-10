e9b27ed75bc84cbc6b73ea3b5117e30ca60b5af3
Kyle Barthelmes <kbart2401@gmail.com>
2021-03-10 13:41:04 -0500

    removed fx.js file while still in dev

--- assets/js/fx.js
+++ assets/js/fx.js
@@ -1,19 +1,19 @@
-window.addEventListener('DOMContentLoaded', () => {
+// window.addEventListener('DOMContentLoaded', () => {
 
-  let state = false
+//   let state = false
 
-  document.querySelectorAll('.image').forEach((div) => {
-    div.addEventListener('mouseover', (e) => {
-      if (state === false) {
-        let message = document.createElement('div')
-        div.appendChild(message)
-        message.innerHTML = 'Visit Live Site'
-        state = true
-      } else {
-        e.target.innerHTML = ''
-        state = false
-      }
-    })
+//   document.querySelectorAll('.image').forEach((div) => {
+//     div.addEventListener('mouseover', (e) => {
+//       if (state === false) {
+//         let message = document.createElement('div')
+//         div.appendChild(message)
+//         message.innerHTML = 'Visit Live Site'
+//         state = true
+//       } else {
+//         e.target.innerHTML = ''
+//         state = false
+//       }
+//     })
 
-  })
-})
+//   })
+// })
